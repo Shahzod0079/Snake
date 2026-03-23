14eb4501c5330e0cd3e74f973ac35cac8d0f32e0
student-v424 <student-v424@STUD-RM6-V424>
2026-03-23 13:51:15 +0300

    Создание классов Leaders, ViewModelGames, ViewModelUserSettings и метода Send. Тошматов

--- Snake/Program.cs
+++ Snake/Program.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using Common;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -20,6 +23,35 @@ namespace Snake
 
         static void Main(string[] args)
         {
+        }
+        private static void Send()
+        {
+            foreach (ViewModelUserSettings User in remoteIPAddress)
+            {
+
+                UdpClient sender = new UdpClient();
+
+                IPEndPoint endPoint = new IPEndPoint(
+                    IPAddress.Parse(User.IpAddress),
+                    int.Parse(User.Port));
+                try
+                {
+                    byte[] bytes = Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(viewModelGames.Find(x => x.IdSnake == User.IdSnake)));
+
+                    sender.Send(bytes, bytes.Length, endPoint);
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.WriteLine($"Отправил данные пользователю: {User.IpAddress}:{User.Port}");
+                }
+                catch (Exception ex)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Возникло исключение: " + ex.ToString() + "\n" + ex.Message);
+                }
+                finally
+                {
+                    sender.Close();
+                }
+            }
         }
     }
 }
