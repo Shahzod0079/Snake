258eaf8afcbfa6ccf0cea8c427ef03b11c85d9da
Shahzod0079 <toshmatovshahzod69@gmail.com>
2026-03-28 15:17:59 +0500

    Добавленные методы для главной страницы. Тошматов

--- SnakeWPF/MainWindow.xaml.cs
+++ SnakeWPF/MainWindow.xaml.cs
@@ -1,7 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
+using System.Linq.Expressions;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -9,9 +14,11 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Common;
 
 namespace SnakeWPF
 {
@@ -20,9 +27,124 @@ namespace SnakeWPF
     /// </summary>
     public partial class MainWindow : Window
     {
+        public static MainWindow mainWindow;
+        public ViewModelUserSettings viewModelUserSettings = new ViewModelUserSettings();
+        public ViewModelGames ViewModelGames = null;
+        public static IPAddress remoteIPADress = IPAddress.Parse("127.0.0.1");
+        public static int remotePort = 5001;
+        public Thread tRec;
+        public UdpClient receivingUdpClient;
+        public Pages.Home Home = new Pages.Home();
+        public Pages.Game Game = new Pages.Game();
+
+
         public MainWindow()
         {
             InitializeComponent();
+            mainWindow = this;
+            OpenPage(Home);
+        }
+
+        public void StartReceiver()
+        {
+            tRec = new Thread(new ThreadStart(Receiver));
+            tRec.Start();
+        }
+        public void OpenPage()
+        {
+            DoubleAnimation startAnimation = new DoubleAnimation();
+            startAnimation.From = 1;
+            startAnimation.To = 0;
+            startAnimation.Duration = TimeSpan.FromSeconds(0.6);
+            startAnimation.Completed += delegate
+            {
+                Frame.Navigate(PageOpen);
+                DoubleAnimation endAnimation = new DoubleAnimation();
+                endAnimation.From = 0;
+                endAnimation.To = 1;
+                endAnimation.Duration = TimeSpan.FromSeconds(0.6);
+                frame.BeginAnimation(OpacityProperty, endAnimation);
+            };
+            frame.BeginAnimation(OpacityProperty, startAnimation);
+        }
+        public void Receiver()
+        {
+            receivingUdpClient = new UdpClient(int.Parse(ViewModelUserSettings.Port);
+            IPEndPoint RemoteIpEndPoint = null;
+
+            try
+            {
+                while (true)
+                {
+                    byte[] receiveBytes = receivingUdpClient.Receive(
+                        ref RemoteIpEndPoint,);
+
+                    string returndata = Encoding.UTF8.GetString(receiveBytes);
+                    if (returndata != null)
+                    {
+                        Dispatcher.Invoke(() =>
+                        OpenPage(Game);
+                    });
+                }
+                ViewModelGames = JsonConvert.DeserializeObject<ViewModelGames>(returndata.ToString());
+                if (ViewModelGames.SnakesPlayers.GameOver)
+                {
+                    Dispatcher.Invoke(() =>
+                    {
+                        OpenPage(new Pages.EndGame());
+                    });
+
+                }
+                else
+                {
+                    Game.CreateUI();
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine("Возникло исключение: " + ex.ToString() + "\n" + ex.Message);
+            }
+        }
+        public static void Send()
+        {
+            UdpClient sender = new UdpClient();
+            IPEndPoint endPoint = new IPEndPoint(remoteIPADress, remotePort);
+            try
+            {
+                Byte[] byres = Encoding.UTF8.GetBytes(datagram);
+                sender.Send(bytes, bytes.Length, endPoint)
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine("Возникло исключение: " + ex.ToString() + "\n" + ex.Message);
+            }
+            finally
+            {
+                sender.Close();
+            }
+        }
+        public void EvenKeyUp()
+        {
+            if (!string.IsNullOrEmpty(ViewModelUserSettings.IPAddress) && 
+                !string.IsNullOrEmpty(ViewModelUserSettings.Port) &&
+                (ViewModelGames != null && !ViewModelGames.SnakesPlayers.GameOver))
+            {
+                if (e.Key == Key.Up)
+                    Send($"Up|{JsonConvert.SerializeObject(ViewModelUserSettings)}");
+                else if (e.Key == Key.Down)
+                    Send($"Down|{JsonConvert.SerializeObject(ViewModelUserSettings)}");
+                if (e.Key == Key.Left)
+                    Send($"Left|{JsonConvert.SerializeObject(ViewModelUserSettings)}");
+                else if (e.Key == Key.Right)
+                    Send($"Right|{JsonConvert.SerializeObject(ViewModelUserSettings)}");
+            }
+        }
+        private void QuitApplication(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            receivingUdpClient.Close();
+            tRec.Abort();
         }
     }
 }
+    
+
